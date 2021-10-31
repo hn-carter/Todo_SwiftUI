e9d451b3fb7dee719d713d947af4b631923ffaab
hn-carter-mac <84824787+hn-carter@users.noreply.github.com>
2021-10-31 23:50:00 +0900

    upd 日付処理

--- Todo_SwiftUI/Models/Todo.swift
+++ Todo_SwiftUI/Models/Todo.swift
@@ -14,21 +14,23 @@ struct Todo: Identifiable, Codable {
     let id: UUID
     var title: String
     var isComplete: Bool
+    var dueDate: Date
     var created: Date
     
-    init(id: UUID = UUID(), title: String, isComplete: Bool = false, created: Date = Date()) {
+    init(id: UUID = UUID(), title: String, isComplete: Bool = false, dueDate: Date = Date(), created: Date = Date()) {
         self.id = id
         self.title = title
         self.isComplete = isComplete
+        self.dueDate = dueDate
         self.created = created
     }
     
     // プレビュー用サンプルデータ
     static var data: [Todo] {
         [
-            Todo(title: "iMacの電源を入れる", isComplete: true),
-            Todo(title: "Xcodeを起動する", isComplete: true),
-            Todo(title: "なんかすごいあぷりをつくる")
+            Todo(title: "iMacの電源を入れる", isComplete: true, dueDate: Date()),
+            Todo(title: "Xcodeを起動する", isComplete: true, dueDate: Date(timeIntervalSinceNow: 3600)),
+            Todo(title: "なんかすごいあぷりをつくる", dueDate: Date(timeIntervalSinceReferenceDate: 365))
         ]
     }
 }
